46d1e1b112e3f2cd8138ed7e140ca4a047288e2f
Joel Shotton <joel.shotton@deakin.edu.au>
2020-05-22 18:51:35 +1000

    Working on highlighting text, token list is working

--- question-generation-app/src/app/pages/quiz/quiz.component.ts
+++ question-generation-app/src/app/pages/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChipList } from '@syncfusion/ej2-angular-buttons';
 
 @Component({
   selector: 'app-quiz',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
-  private tokens = ["test 1", "test 2"];
+  @ViewChild('chipsList') public chipsList: ChipList;
+
+  private tokens: String[] = ["circulatory", "cardiovascular"];
+  public textValue: String = "The circulatory system, also called the cardiovascular system or the vascular system, is an organ system that permits blood to circulate and transport nutrients (such as amino acids and electrolytes), oxygen, carbon dioxide, hormones, and blood cells to and from the cells in the body to provide nourishment and help in fighting diseases, stabilize temperature and pH, and maintain homeostasis.";
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    
+  }
+
+
+  newToken(token : String) {
+    this.tokens.push(token);
+    this.chipsList.refresh();
+  }
+
+  deleteToken({text, index}) {
+    
+    console.log("deleted", this.tokens, text, index);
+  }
 }
--- question-generation-app/src/app/pages/quiz/text-editor/text-editor.component.ts
+++ question-generation-app/src/app/pages/quiz/text-editor/text-editor.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Injectable, ViewChild, Input } from '@angular/core';
+import { Component, OnInit, Injectable, ViewChild, Input, Output, EventEmitter, NgZone } from '@angular/core';
 import { ToolbarService, LinkService, ImageService, HtmlEditorService, PasteCleanupService, 
-          RichTextEditorComponent } from '@syncfusion/ej2-angular-richtexteditor';
+          RichTextEditorComponent, CommandName } from '@syncfusion/ej2-angular-richtexteditor';
 
 @Component({
   selector: 'app-text-editor',
@@ -19,8 +19,9 @@ import { ToolbarService, LinkService, ImageService, HtmlEditorService, PasteClea
 })
 export class TextEditorComponent implements OnInit {
 
-  @Input('value') value: String = "dd"; 
+  @Input('textValue') textValue: String; 
   @Input('tokens') tokens: String[];
+  @Output() newToken = new EventEmitter();
 
   @ViewChild('toolsRTE') public rteObj: RichTextEditorComponent;
 
@@ -44,13 +45,58 @@ export class TextEditorComponent implements OnInit {
   }
 
   private placeholder = `
-  
-    <table border="0"><tr><td>Hello</td><td>Hello</td></tr></table>
-  
+    <table style="border: none; width: 100%;">
+      <tr>
+        <td style="border: none; text-align: center;" colspan="2"> 
+          <h3>Simply copy and paste text here…</h3>
+          <p>Here’s a few tips before you start:</p>
+        </td>
+      </tr>
+      <tr>
+        <td style="border: none; width: 50%;">
+          <p>Quiz Generator absolutely loves fact-based content, even if it’s quite complex…</p>
+        </td>
+        <td style="border: none; width: 50%; text-align: right;">
+          <p>…but struggles with content that might be too cryptic, metaphorical or inferential.</p>
+        </td>
+      </tr>
+      <tr>
+        <td style="border: none; text-align: center;">
+          <img src="/assets/icon-happy.png"/>
+        </td>
+        <td style="border: none; text-align: center;">
+          <img src="/assets/icon-happy.png"/>
+        </td>
+      </tr>
+      <tr>
+        <td style="border: none; width: 50%;">
+          <p>Example: “The circulatory system, also called the cardiovascular system or the vascular system, is an organ system that permits blood to circulate and transport nutrients (such as amino acids and electrolytes), oxygen, carbon dioxide, hormones, and blood cells to and from the cells in the body to provide nourishment and help in fighting diseases, stabilize temperature and pH, and maintain homeostasis.”</p>
+        </td>
+        <td style="border: none; width: 50%; text-align: right;">
+          <p>Example: “They have tied me to a stake. I cannot fly,
+              <br/>
+              But, bearlike, I must fight the course. What’s he
+              <br/>
+              That was not born of woman? Such a one
+              <br/>
+              Am I to fear, or none..
+              <br/>
+              <br/>
+              -William Shakespeamre
+              <br/>newToken
+        </td>
+      </tr>
+    </table>
   `;
 
 
 
+  // public customHTMLModel: IHtmlFormatterModel = { // formatter is used to configure the custom key
+  //   keyConfig: {
+  //   'insert-link': 'ctrl+q', // confite the desired key
+  //   }
+  //   };
+  //   public formatter: any = new HTMLFormatter(this.customHTMLModel); // to configure custom key
 
   constructor() { 
    
@@ -61,14 +107,31 @@ export class TextEditorComponent implements OnInit {
   }
 
   ngAfterViewInit(): void {
-
-
-      
     this.textArea = this.rteObj.contentModule.getEditPanel() as HTMLElement; 
     this.textArea.onclick = (event) => {
-      console.log("click 2", this.rteObj.getSelection(), this.rteObj.getText());
-      // rteObj.executeCommand()
+      console.log("click 2", this.rteObj.getSelection(), this.rteObj.getHtml(), 
+                              this.rteObj.getRange());
+      let selection : String =  this.rteObj.getSelection();
+      if (selection.length > 0) {
+        
+        this.rteObj.executeCommand("backColor", "#ff0000");
+        this.newToken.emit(selection);
+      }
     }
+    // let range : Range = new Range();//this.rteObj.getRange();
+    // let rootNode = this.rteObj.element.getRootNode();
+    // range.setStart(this.rteObj.element, 29);
+    // range.setEnd(this.rteObj.element, 35);
+    // this.rteObj.element.querySelectorAll("*").forEach((e) => {
+    //   console.log("e", e.textContent);
+    // });
+    // console.log('range', this.rteObj.getRange());
+    // this.rteObj.selectRange(range)
+    // range.setStart(range.surroundContents)
+    // range.setSsetStartAfter() = 20;
+    // range.startOffset = 20;   
+    // this.rteObj.selectRange()
+    // this.rteObj.executeCommand("backColor", "#ff0000")
   }
 
   editorEvent(event): void {
