02c44c5665d17d89131811c8f8c2ad3569ccbe00
samandeep <amandeep.singh@deakin.edu.au>
2020-07-17 00:35:14 +1000

    pagination with url

--- question-generation-app/src/app/pages/quiz/quiz-list.component.ts
+++ question-generation-app/src/app/pages/quiz/quiz-list.component.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 import { IQuizList } from 'src/app/interfaces/IQuizList';
 import { IQuiz } from 'src/app/interfaces/iQuiz';
 import { QuizService } from 'src/app/services/quiz.service';
-import { Router } from '@angular/router'; // CLI imports router
+import { Router, ActivatedRoute } from '@angular/router'; // CLI imports router
 
 @Component({
   selector: 'app-quiz-list',
@@ -14,16 +14,34 @@ export class QuizListComponent implements OnInit {
   
   public quizList: Observable<IQuizList>;// = { list: IQuiz[] };
 
-  //Pagination initial page & change events
-  page: 1
   //search Quiz (ng model)
   searchQuiz;
+  //pagination config
+  config: any;
 
-  constructor(private _quizService: QuizService, private router: Router) {}
+  constructor(private _quizService: QuizService, private router: Router, private activatedRoute: ActivatedRoute) {
+
+    //set the config
+    this.config = {
+      currentPage: 1,
+      itemsPerPage : 6,
+      totalItems : 0
+    }
+
+    activatedRoute.queryParams.subscribe(
+      params => this.config.currentPage = params['page'] ? params['page']:1
+    )
+
+  }
 
   ngOnInit() {
     this.quizList = this._quizService.listQuizzes();
   }
+
+  //Pagination change event
+  pageChange(newPage: number){
+    this.router.navigate([''], {queryParams : {page: newPage}});
+   }
 
   // newQuiz() {
   //   this.router.navigate(["edit-quiz"]);
